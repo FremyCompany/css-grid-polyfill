5ff251ad3436cfa6bd8e047d62aaa8699af3261e
François REMY <fremycompany_pub@yahoo.fr>
2015-02-27 18:59:51 +0100

    Added: some comments to match my usual style

--- test/demo-css-grid-examples.js
+++ test/demo-css-grid-examples.js
@@ -7,31 +7,49 @@ var dirname = require("path").dirname;
 var basename = require("path").basename;
 var extname = require("path").extname;
 
-var list = [
-	{relativePath: "demo/css-grid/example2.html"}
-];
-
+// file path to the root directories
 var root = resolve(__dirname, "..");
 var tmp = resolve(__dirname, "tmp");
 
-list.forEach(function(o) {
+// some other constants
+var ACCEPTABLE_TOLERANCE = 2.0/*percents*/;
+
+// pages which should be compared to a png
+var testsToRun = 
+[
+	{relativePath: "demo/css-grid/example2.html"}
+];
+
+// execute all the tests in sequence
+testsToRun.forEach(function(o) {
+
+	// extract data about the page to test	
 	var baseFilename = basename(o.relativePath);
 	var extension = extname(o.relativePath);
 	var filename = baseFilename.slice(0, -extension.length);
 	
+	// configure the test
 	o.filePath = resolve(root, o.relativePath);
 	o.screenshotPath = resolve(tmp, o.filePath.slice(root.length + 1).slice(0, -extension.length) + ".png");
 	o.expectedImagePath = resolve(dirname(o.filePath), filename + ".png");
 	o.binfile = resolve(root, "bin/css-polyfills.js");
 
+	// execute the test
 	test(baseFilename, function(t) {
 		compare(o, function onComplete(data) {
+			
+			// check whether the test passed
 			var misMatchPercentage = data.misMatchPercentage;
-			var match = (100 - misMatchPercentage).toFixed(1);
-			var isOK = misMatchPercentage <= 2.0;
+			var isOK = misMatchPercentage <= ACCEPTABLE_TOLERANCE;
+			
+			// remove valid screenshots from the file system
 			if (isOK) fs.unlink(o.screenshotPath);
-			t.ok(isOK, "match percentage " + match);
+
+			// report the test result
+			var match = (100 - misMatchPercentage).toFixed(1);
+			t.ok(isOK, "match percentage: " + match + "%");
 			t.end();
+			
 		});
 	});
 });
