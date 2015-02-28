3b85cb7ac0d96722276565b5aee902ce7a897def
Roman <s91630277@gmail.com>
2015-02-28 03:28:21 +0300

    Reformatted compare.js

--- test/helpers/compare.js
+++ test/helpers/compare.js
@@ -18,8 +18,11 @@ module.exports = function(data, callback) {
 
 	// create our state machine
 	// (we only want to start comparing once the two files are ready)
-	var emitter = new (require("events").EventEmitter);
-	var files = { expected:null, result:null };
+	var emitter = new(require("events").EventEmitter);
+	var files = {
+		expected: null,
+		result: null
+	};
 	// set screen width and height
 	if (data.viewportWidth && data.viewportHeight) {
 		nightmare.viewport(data.viewportWidth, data.viewportHeight);
@@ -27,29 +30,29 @@ module.exports = function(data, callback) {
 	// initiate by taking a screenshot
 	nightmare
 		.goto(filePath)
-		.inject("js", binfile)                                      // FIXME: is it actually required? why?
+		.inject("js", binfile) // FIXME: is it actually required? why?
 		.screenshot(screenshotPath)
 		.run(function(err, nightmare) {
-			
+
 			// continue by reading the taken screenshot
 			if (err) throw err;
 			fs.readFile(screenshotPath, function(err, data) {
 				emitter.emit("fileready", "result", data);
 			});
-			
+
 		});
-	
+
 	// then start reading the expected screenshot
 	fs.readFile(expectedImagePath, function(err, data) {
 		emitter.emit("fileready", "expected", data);
 	});
-	
+
 	// save the read screenshots, then trigger comparison
 	emitter.on("fileready", function(name, data) {
-		
+
 		// save the received screenshot in the pool
 		files[name] = data;
-		
+
 		// once both files are there, start to compare
 		if (files.result && files.expected) {
 			emitter.emit("ready", files);
